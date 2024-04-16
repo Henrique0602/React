53d6f073248a61dafd594597802295283ea4df51
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-16 12:11:25 -0300

    Pagina HOME

---
+++ Projeto-AT/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from'./Componentes/Header/header'
+import Main from './Componentes/Main/MainTitle'
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ Projeto-AT/src/Componentes/Header/header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+    return(
+
+    <header>
+        <nav>
+           <a href="#" className="active" >Home</a>
+           <a href="#">Sobre</a>
+           <a href="#">Contato</a>
+        </nav>
+    </header>
+
+
+        
+    )
+}
+
+export default Header
