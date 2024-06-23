d9c915e1d635d92cb0b7a6c8d6c6763ac6f5aee7
Henrique0602 <henriquef.brigo@gmail.com>
2024-06-23 19:04:26 -0300

    AC FILMES

--- cineminha/src/Pages/Filmes.jsx
+++ cineminha/src/Pages/Filmes.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom"
+import { FaCheck } from "react-icons/fa";
+import { FaRegTimesCircle } from "react-icons/fa";
 
 
 
@@ -22,23 +24,33 @@ function Filmes() {
 
     return (
 
+
         <>
-            <div className="listaFilmes bg-black text-white flex flex-col gap-4">
-                {filmes.map((filme) => (
-                    <div className="card-filme m-10 flex" key={filme.id}>
-                        <div className="flex flex-col justify-center items-center">
-                            <h1 className="rounded-lg border-solid border-2 border-red-600 w-80 mb-5 p-3 text-center">{filme.title}</h1>
-                            <img className="w-32" src={`${urlImg}${filme.poster_path}`} alt={filme.title} />
+          
+            <div className="listaFilmes bg-black text-white flex flex-row gap-4">
+                <div className="grid grid-cols-3 gap-4 ">
+                    {filmes.map((filme) => (
+                        <div className="card-filme m-10 flex flex-col bg-gray-800 p-5 rounded-lg mx-5 gap-3" key={filme.id}>
+                            
+                            <div className="flex flex-col justify-center items-center">
+                                <h1 className="rounded-lg border-solid border-2 border-red-600 w-80 mb-5 p-3 text-center">{filme.title}</h1>
+                                <img className="w-[250px] flex justify-center items-center m-10 rounded-lg ml-16 mb-14 mt-10" src={`${urlImg}${filme.poster_path}`} alt={filme.title} />
+                            </div>
+                            <div className="flex flex-col ml-5 justify-center items-center">
+                                <p className="rounded-lg border-solid border-2 border-white mb-5 p-3 text-justify w-10/12">{filme.overview}</p>
+                                <button className="bg-red-800 w-40 h-10 rounded-xl ">
+                                    <Link to={`${filme.id}`}>Saber Mais</Link>
+                                </button>
+                               
+                            </div>
+                            <FaCheck className="text-white mt-5 text-2xl cursor-pointer hover:text-green-500 transition-colors flex justify-end" />
+                            <FaRegTimesCircle  className="text-white mt-5 text-2xl cursor-pointer hover:text-red-500 transition-colors flex justify-end" />
                         </div>
-                        <div className="flex flex-col ml-5 justify-center items-center"> 
-                            <p className="rounded-lg border-solid border-2 border-white mb-5 p-3 text-justify w-10/12">{filme.overview}</p>
-                            <button className="bg-red-800 w-40 h-10 rounded-xl ">
-                                <Link to={`${filme.id}`}>Saber Mais</Link>
-                            </button>
-                        </div>
+                    
+                    ))}
                     </div>
-                ))}
-            </div>
+                </div>
+            
         </>
 
     );
--- cineminha/src/Pages/Home.jsx
+++ cineminha/src/Pages/Home.jsx
@@ -1,38 +1,50 @@
 import data from "../../article.json"
+import { FaRegHeart } from "react-icons/fa";
+
+
 
 
 function Home() {
-    console.log(data)
+
     return (
         <main className="bg-black">
             <div className="flex justify-center ">
                 <input className=" w-80 h-12 rounded-lg mt-10 p-5 border-solid border-2 border-white bg-black text-white" type="text" name="busca" id="busca" placeholder="Digite aqui sua busca" />
             </div>
+            <h1 className="text-white font-bold text-2xl flex justify-center mb-10 mt-20 ">Não fique por fora. Saiba de tudo!!</h1>
 
             <div className="grid grid-cols-3 gap-4">
 
+
                 {data.map(
                     filme => (
-                        <div className="card mx-5 gap-3" key={filme.title}>
-                            <h1 className="bg-black text-white flex justify-center border-solid border-2 border-white rounded-lg mt-5 ">{filme.title}</h1>
+                        <div className="card bg-gray-800 p-5 rounded-lg mx-5 gap-3 ">
 
-                            {filme.image ?
-                                <img className="w-[250px] flex justify-center items-center m-10 rounded-lg mb-15 mt-10  " src={filme.image} alt={filme.title} /> :
-                                "Não tem imagem"}
+                            <div className="card mx-5 gap-3" key={filme.title}>
+                                <h1 className="bg-black text-white flex justify-center border-solid border-2 border-white rounded-lg mt-5 ">{filme.title}</h1>
 
-                            <div className="text bg-black rounded-lg text-white border-solid border-2 p-10 mb-7 text-justify">
-                                {filme.text.map(text => (
-                                    <span key={text}> {text}</span>
-                                ))}
+                                {filme.image ?
+                                    <img className="w-[250px] flex justify-center items-center m-10 rounded-lg ml-16 mb-14 mt-10  " src={filme.image} alt={filme.title} /> :
+                                    "Não tem imagem"}
+
+                                <div className="text bg-black rounded-lg text-white border-solid border-2 p-10 mb-7 text-justify">
+                                    {filme.text.map(text => (
+                                        <span key={text}> {text}</span>
+                                    ))}
+                                </div>
+
+                                <div className="tag bg-black rounded-lg text-white flex justify-center border-solid border-2 mt-4" >
+                                    {filme.tags.map(tag => (
+                                        <span key={tag}> {tag}</span>
+                                    ))}
+                                </div>
+
+                              
+
+
+
                             </div>
-
-                            <div className="tag bg-black rounded-lg text-white flex justify-center border-solid border-2 mt-4" >
-                                {filme.tags.map(tag => (
-                                    <span key={tag}> {tag}</span>
-                                ))}
-                            </div>
-
-
+                            <FaRegHeart className="text-white mt-5 text-2xl cursor-pointer hover:text-red-500 transition-colors flex justify-end" />
                         </div>
 
                     )
